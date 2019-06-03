efb760139de8c7a152a5926900e978eae9a49613
Spencer Schoeben <spencer@netspencer.com>
2019-06-02 22:42:58 -0700

    basic retry on deployment until ready

--- src/endpoints/hooks/deployment.ts
+++ src/endpoints/hooks/deployment.ts
@@ -1,15 +1,23 @@
 import { IncomingMessage, ServerResponse } from 'http'
 import { json, send } from 'micro'
 import fetch from '../../utils/fetch'
+import { Headers } from 'node-fetch'
 import * as mongoose from 'mongoose'
-import { ConfigurationModel, DeploymentModel } from '../../models'
-import { Snapshot } from '../../models/snapshot'
+import {
+  Snapshot,
+  ConfigurationModel,
+  DeploymentModel,
+  Token
+} from '../../models'
+
+const sleep = (ms: number): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 mongoose.connect(process.env.MONGO_URL || '', { useNewUrlParser: true })
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
   const data: { [key: string]: any } = await json(req)
-  console.log(data)
 
   const deploymentId = data.payload.deploymentId
   const url = data.payload.url
@@ -17,20 +25,23 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
 
   const d = new DeploymentModel()
 
-  if (data.userId) {
-    const config = await ConfigurationModel.findOne({
-      'token.userId': userID
-    })
-    if (config) {
-      d.url = url
-      d.createdAt = data.createdAt
-      d.deploymentId = deploymentId
-      await d.save()
-
-      await config.updateOne({ $push: { deployments: d } })
-    }
-    console.log('configuration', config)
+  const config = await ConfigurationModel.findOne({
+    'token.userId': userID
+  })
+  if (!config) {
+    return send(res, 409)
   }
+
+  if (config) {
+    d.url = url
+    d.createdAt = data.createdAt
+    d.deploymentId = deploymentId
+    await d.save()
+
+    await config.updateOne({ $push: { deployments: d } })
+  }
+  const token = config.token as Token
+  await checkDeploymentReady(token.access_token || '', deploymentId)
 
   const snapshot = await fetch(process.env.BASE_URL + '/hooks/snapshot', {
     method: 'POST',
@@ -50,4 +61,34 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
   await d.save()
 
   send(res, 200, data)
+}
+
+const checkDeploymentReady = async (
+  token: string,
+  deploymentId: string
+): Promise<boolean> => {
+  const url = `https://api.zeit.co/v9/now/deployments/${deploymentId}`
+  const headers = new Headers()
+  headers.set('Authorization', `Bearer ${token}`)
+
+  let deployment: { [key: string]: any }
+  while (true) {
+    const data = await fetch(url, {
+      method: 'GET',
+      headers
+    })
+    deployment = await data.json()
+    console.log('deployment', deployment)
+
+    if (
+      !deployment.readyState ||
+      deployment.readyState == 'READY' ||
+      deployment.readyState == 'ERROR'
+    ) {
+      break
+    }
+    await sleep(3000)
+  }
+
+  return false
 }
