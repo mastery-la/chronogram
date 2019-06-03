90aa6f690a9c390c62071137edd0618af018f9f6
Spencer Schoeben <spencer@netspencer.com>
2019-06-03 00:42:18 -0700

    change viewport

--- src/utils/screenshot.ts
+++ src/utils/screenshot.ts
@@ -6,14 +6,19 @@ export async function getScreenshot(
   type: 'png' | 'jpeg' = 'png'
 ): Promise<Buffer> {
   const browser = await puppeteer.launch({
+    defaultViewport: {
+      width: 1460,
+      height: 980
+    },
     args: chrome.args,
     executablePath: await chrome.executablePath,
     headless: chrome.headless
   })
 
   const page = await browser.newPage()
+  page.browser
   await page.goto(url)
-  const file = await page.screenshot({ type })
+  const file = await page.screenshot({ type, fullPage: true })
   await browser.close()
   return file
 }
